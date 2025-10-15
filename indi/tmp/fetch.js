d74a86e0e08f22ab5888e03724d2e2b385f9c1a8
ChenHQ <huiqi.chen@dbappsecurity.com.cn>
2025-10-15 09:15:33 +0800

    feat: copy from browser

---
+++ indi/tmp/fetch.js
@@ -0,0 +1,30 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+async function getData() {
+    try {
+        res = await fetch("https://10.50.3.44:4443/_data-route/data-asset/catalogQuery", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "zh-CN,zh;q=0.9",
+                "content-type": "application/json",
+                "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"macOS\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-tenant-schema": "tenant_1758519122706",
+                "cookie": "SESSION=YWUxZjhkYmMtMjU1NS00YWY4LWE4MjYtNDg4MDIxMjEzNDc1",
+                "Referer": "https://10.50.3.44:4443/"
+            },
+            "body": "{\"filters\":[],\"orders\":[],\"page\":1,\"size\":20,\"type\":\"1\"}",
+            "method": "POST"
+        });
+        const result = await res.json(); // 解析 JSON 响应
+        console.log("查询结果：", result);
+    }catch (error) {
+
+    }
+}
+
+getData()
